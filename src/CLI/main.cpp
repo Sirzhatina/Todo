b569c81484bf2c885b32c0421af16e711219c28a
Sirzhatina <mazurkevichsergey@gmail.com>
2024-06-22 11:22:40 +0300

    let's try to develop from the trivial prototype as a start point

--- src/CLI/main.cpp
+++ src/CLI/main.cpp
@@ -1,11 +1,22 @@
-#include <Core/IClient.hpp>
-#include "Client.hpp"
-#include <memory>
+#include <iostream>
+#include <boost/program_options.hpp>
 
 int main(int argc, char* argv[]) {
-    std::unique_ptr<IClient> app = std::make_unique<Client>();
+    namespace po = boost::program_options;
 
-    app->parseCmd(argc, argv);
+    if (argc < 2) {
+        std::cerr << "Usage: program command [options]\n";
+        return EXIT_FAILURE;
+    }
 
-    return static_cast<int>(app->run());
+    if (std::string{argv[1]} != "add") {
+        std::cerr << "Unknown command\n";
+        return EXIT_FAILURE;
+    }
+
+    po::options_description descr{"Allowed options"};
+
+    descr.add_options()
+        ("name,n", "Task name")
+        ("description,d", "Task description");
 }
